97bed54ded09c89e62762081a6ad5734e935b7e4
DarkWeird <foxflameless@gmail.com>
2020-09-09 17:06:42 +0300

    feature(reintegrate-gestaltv7):  gestalt-v7 compability (package)

--- src/main/java/org/terasology/StructuralResources/IngotStackSystem.java
+++ src/main/java/org/terasology/StructuralResources/IngotStackSystem.java
@@ -1,50 +1,37 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.StructuralResources;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.audio.events.PlaySoundEvent;
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.EventPriority;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.logic.characters.events.AttackEvent;
-import org.terasology.logic.common.ActivateEvent;
-import org.terasology.logic.inventory.InventoryManager;
-import org.terasology.logic.inventory.InventoryUtils;
-import org.terasology.logic.inventory.SelectedInventorySlotComponent;
-import org.terasology.logic.inventory.events.BeforeItemPutInInventory;
-import org.terasology.logic.inventory.events.InventorySlotChangedEvent;
-import org.terasology.logic.inventory.events.InventorySlotStackSizeChangedEvent;
-import org.terasology.logic.location.LocationComponent;
-import org.terasology.logic.players.LocalPlayer;
-import org.terasology.math.ChunkMath;
-import org.terasology.math.Side;
+import org.terasology.engine.audio.events.PlaySoundEvent;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.event.EventPriority;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.logic.characters.events.AttackEvent;
+import org.terasology.engine.logic.common.ActivateEvent;
+import org.terasology.engine.logic.location.LocationComponent;
+import org.terasology.engine.logic.players.LocalPlayer;
+import org.terasology.engine.math.ChunkMath;
+import org.terasology.engine.math.Side;
+import org.terasology.engine.registry.In;
+import org.terasology.engine.utilities.Assets;
+import org.terasology.engine.world.BlockEntityRegistry;
+import org.terasology.engine.world.WorldProvider;
+import org.terasology.engine.world.block.Block;
+import org.terasology.engine.world.block.BlockComponent;
+import org.terasology.engine.world.block.BlockManager;
+import org.terasology.engine.world.block.entity.placement.PlaceBlocks;
+import org.terasology.engine.world.block.family.BlockFamily;
+import org.terasology.inventory.logic.InventoryManager;
+import org.terasology.inventory.logic.InventoryUtils;
+import org.terasology.inventory.logic.SelectedInventorySlotComponent;
+import org.terasology.inventory.logic.events.BeforeItemPutInInventory;
+import org.terasology.inventory.logic.events.InventorySlotChangedEvent;
+import org.terasology.inventory.logic.events.InventorySlotStackSizeChangedEvent;
 import org.terasology.math.geom.Vector3i;
-import org.terasology.registry.In;
-import org.terasology.utilities.Assets;
-import org.terasology.world.BlockEntityRegistry;
-import org.terasology.world.WorldProvider;
-import org.terasology.world.block.Block;
-import org.terasology.world.block.BlockComponent;
-import org.terasology.world.block.BlockManager;
-import org.terasology.world.block.entity.placement.PlaceBlocks;
-import org.terasology.world.block.family.BlockFamily;
 
 @RegisterSystem
 public class IngotStackSystem extends BaseComponentSystem {
@@ -95,7 +82,8 @@ public class IngotStackSystem extends BaseComponentSystem {
             PlaceBlocks placeNewIngotStack = new PlaceBlocks(targetPos, newStackBlock, instigator);
             worldProvider.getWorldEntity().send(placeNewIngotStack);
             instigator.send(new PlaySoundEvent(Assets.getSound("engine:PlaceBlock").get(), 0.5f));
-            inventoryManager.moveItem(instigator, instigator, findSlot(instigator), blockEntityRegistry.getBlockEntityAt(targetPos), 0, 1);
+            inventoryManager.moveItem(instigator, instigator, findSlot(instigator),
+                    blockEntityRegistry.getBlockEntityAt(targetPos), 0, 1);
             updateIngotStack(targetPos, 1, instigator);
         }
         event.consume();
@@ -113,12 +101,14 @@ public class IngotStackSystem extends BaseComponentSystem {
 
     // for real-time updates to the stack
     @ReceiveEvent
-    public void onStackSizeChange(InventorySlotStackSizeChangedEvent event, EntityRef stackEntity, IngotStackComponent stackComponent) {
+    public void onStackSizeChange(InventorySlotStackSizeChangedEvent event, EntityRef stackEntity,
+                                  IngotStackComponent stackComponent) {
         EntityRef instigator = localPlayer.getCharacterEntity();
         LocationComponent locationComponent = stackEntity.getComponent(LocationComponent.class);
         Vector3i pos = new Vector3i(locationComponent.getWorldPosition());
         if (event.getNewSize() > MAX_INGOTS) {
-            inventoryManager.moveItem(stackEntity, instigator, 0, instigator, findSlot(instigator), event.getNewSize() - MAX_INGOTS);
+            inventoryManager.moveItem(stackEntity, instigator, 0, instigator, findSlot(instigator),
+                    event.getNewSize() - MAX_INGOTS);
         }
         updateIngotStack(pos, event.getNewSize(), instigator);
     }
